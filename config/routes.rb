94e6e4ff31349eb6afd3c47fd9d399af0ffbd18f
Mirzalazuardi Hermawan <mirzalazuardi@users.noreply.github.com>
2023-04-07 05:57:37 +0700

    add route to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'users', to: 'users#create'
 end
