2b74ae1b2256fa15d29a91e038243c91ee5f87e2
Mirzalazuardi Hermawan <mirzalazuardi@users.noreply.github.com>
2023-04-07 06:37:41 +0700

    add exception for bad_request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from RequiredFieldMissing, with: :bad_request
+
+  def bad_request(e)
+    render json: {error: 'Required field missing: e'}
+  end
 end
---
+++ app/errors/required_field_missing.rb
@@ -0,0 +1,1 @@
+class RequiredFieldMissing < StandardError; end
