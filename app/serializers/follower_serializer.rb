0b966cbded29be6cfc95c4903ac714edfc8180af
Mirzalazuardi Hermawan <mirzalazuardi@users.noreply.github.com>
2023-04-09 11:10:50 +0700

    update attribute serializers

--- app/serializers/follower_serializer.rb
+++ app/serializers/follower_serializer.rb
@@ -1,4 +1,15 @@
 class FollowerSerializer
   include JSONAPI::Serializer
-  attributes 
+  attributes :user, :follower
+
+  belongs_to :user, serializer: UserSerializer
+  belongs_to :follower, serializer: UserSerializer
+
+  attribute :user do |object|
+    object.user.slice(:id, :name)
+  end
+
+  attribute :follower do |object|
+    object.follower.slice(:id, :name)
+  end
 end
--- app/serializers/sleep_serializer.rb
+++ app/serializers/sleep_serializer.rb
@@ -1,4 +1,14 @@
 class SleepSerializer
   include JSONAPI::Serializer
-  attributes 
+  attributes :user, :start, :finish, :duration
+
+  belongs_to :user, serializer: UserSerializer
+
+  attribute :user do |object|
+    object.user.slice(:id, :name)
+  end
+
+  attribute :duration do |object|
+    Sleep.human_readable_time(object.duration_seconds)
+  end
 end
